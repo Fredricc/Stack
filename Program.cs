1041eb95042789cad3aa5e51d80ca4043507a7c4
Fredrick Njuguna <kfred45.fk@gmail.com>
2024-01-17 21:48:44 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Stack
+{
+    public class Stack
+    {
+        //private object obj;
+
+        //public void Push(object obj)
+        //{
+        //    Stack<int> stack = new Stack<int>();
+        //}
+
+        //public object Pop()
+        //{
+        //    Peek(Push.stack);
+        //    return obj;
+        //}
+
+
+        public void Clear()
+        {
+
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+            //var stack = new Stack();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+        }
+    }
+}
