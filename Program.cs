b495d3545b7c9242d2ae60451c3a271539ccc591
Fredrick Njuguna <kfred45.fk@gmail.com>
2024-01-19 21:19:17 +0300

    Added stack class

--- Program.cs
+++ Program.cs
@@ -1,26 +1,5 @@
 ﻿namespace Stack
 {
-    public class Stack
-    {
-        //private object obj;
-
-        //public void Push(object obj)
-        //{
-        //    Stack<int> stack = new Stack<int>();
-        //}
-
-        //public object Pop()
-        //{
-        //    Peek(Push.stack);
-        //    return obj;
-        //}
-
-
-        public void Clear()
-        {
-
-        }
-    }
     internal class Program
     {
         static void Main(string[] args)
---
+++ Stack.cs
@@ -0,0 +1,24 @@
+﻿namespace Stack
+{
+    public class Stack
+    {
+        //private object obj;
+
+        //public void Push(object obj)
+        //{
+        //    Stack<int> stack = new Stack<int>();
+        //}
+
+        //public object Pop()
+        //{
+        //    Peek(Push.stack);
+        //    return obj;
+        //}
+
+
+        public void Clear()
+        {
+
+        }
+    }
+}
